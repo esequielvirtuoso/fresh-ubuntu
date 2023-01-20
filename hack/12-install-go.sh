16630b71bfebbece1e735ff8ce3dc9998e2d2503
Esequiel Virtuoso <virtuosoesequiel@gmail.com>
2023-01-20 18:22:31 -0300

    Add go installation script.

--- hack/12-install-go.sh
+++ hack/12-install-go.sh
@@ -1,4 +1,23 @@
-echo "**************** Installin vim before install go... ****************"
+echo "**************** Installing vim before install go... ****************"
 sudo apt update && sudo apt upgrade
 sudo apt install vim -y
 vim --version
+echo "**************** Vim successfully installed! ****************"
+
+echo "**************** Removing any previous go version and installing go 1.19.5... ****************"
+wget -c https://go.dev/dl/go1.19.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.19.5.linux-amd64.tar.gz
+
+
+echo "**************** Set go environment variables... ****************"
+cat >> ~/.profile <<x
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin
+x
+source ~/.profile
+echo "**************** Go variables all set! ****************"
+
+go version
+echo "**************** Golang successfully installed! ****************"
